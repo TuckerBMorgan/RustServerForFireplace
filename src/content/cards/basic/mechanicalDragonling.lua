fad476492026aa0bd01d95c583f5248584399bc5
JohnBCasey <caseyjohnb@gmail.com>
2017-08-26 17:55:54 -0400

    mistaken content files removed from src

--- src/content/cards/basic/mechanicalDragonling.lua
+++
@@ -1,6 +0,0 @@
-minion
-@@
-create_minion_function**
-    m = Minion.new("mechanicalDragonling", give_uid, 1, "basic", 2, 1, "Mechanical Dragonling");
-    m:add_tag("Mech")
-    result = m
--- src/content/test.lua
+++
@@ -1,4 +0,0 @@
-s_m = Rune.new_set_mana(10,78)
-s_g = Rune.new_start_game();
-
-result = {RuneTypeEnum.new_set_mana(s_m), RuneTypeEnum}; 
