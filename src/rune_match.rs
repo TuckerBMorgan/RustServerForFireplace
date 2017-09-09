08a694ea000f97a133912b30874ae55252dfbb57
JohnBCasey <caseyjohnb@gmail.com>
2017-09-09 16:33:32 -0400

    No longer breaking on modify hero health. AI now playing eachother

--- src/rune_match.rs
+++ src/rune_match.rs
@@ -22,6 +22,9 @@ use runes::remove_tag::RemoveTag;
 //use runes::create_minion::CreateMinion;
 use runes::summon_minion::SummonMinion;
 use runes::damage_rune::DamageRune;
+use runes::modify_hero_health::ModifyHeroHealth;
+
+
 use rustc_serialize::json;
 use rustc_serialize::json::Json;
 
@@ -33,108 +36,114 @@ pub fn get_rune(json_obj : &str)->Box<Rune>{
 
     println!("Decoding to Boxed Struct : {0}", json_obj);
 
-    match message_type {
-        ref x if x == "\"DealCard\"" =>{
+    match message_type.as_ref() {
+        "\"DealCard\"" =>{
             println!("DealCard found");
             let ns = json_obj.replace("{\"runeType\":\"DealCard\",","{");
             //let obj = j_message.as_object().unwrap();
             let dc : DealCard = json::decode(ns.trim()).unwrap();
             return dc.into_box();
         },
-        ref x if x == "\"NewController\"" =>{
+        "\"NewController\"" =>{
             println!("NewController found");
             let ns = json_obj.replace("{\"runeType\":\"NewController\",","{");
             //let obj = j_message.as_object().unwrap();
             let dc : NewController = json::decode(ns.trim()).unwrap();
             return dc.into_box();
         },
-        ref x if x == "\"StartGame\"" =>{
+        "\"StartGame\"" =>{
             println!("StartGame found");
             let ns = json_obj.replace("{\"runeType\":\"StartGame\",","{");
             //let obj = j_message.as_object().unwrap();
             let dc : StartGame = json::decode(ns.trim()).unwrap();
             return dc.into_box();
         },
-        ref x if x == "\"ShuffleCard\"" =>{
+        "\"ShuffleCard\"" =>{
             println!("ShuffleCard found");
             let ns = json_obj.replace("{\"runeType\":\"ShuffleCard\",","{");
             //let obj = j_message.as_object().unwrap();
             let dc : ShuffleCard = json::decode(ns.trim()).unwrap();
             return dc.into_box();
         },
-        ref x if x ==  "\"SetBaseMana\""=>{
+        "\"SetBaseMana\""=>{
             println!("SetBaseMana found");
             let ns = json_obj.replace("{\"runeType\":\"SetBaseMana\",","{");
             let dc : SetBaseMana = json::decode(ns.trim()).unwrap();
             return dc.into_box();
         },
-        ref x if x ==  "\"SetMana\""=>{
+        "\"SetMana\""=>{
             println!("SetMana found");
             let ns = json_obj.replace("{\"runeType\":\"SetMana\",","{");
             let dc : SetMana = json::decode(ns.trim()).unwrap();
             return dc.into_box();
         },
-        ref x if x ==  "\"RotateTurn\""=>{
+        "\"RotateTurn\""=>{
             println!("RotateTurn found");
             let ns = json_obj.replace("{\"runeType\":\"RotateTurn\",","{");
             let dc : RotateTurn = json::decode(ns.trim()).unwrap();
             return dc.into_box();
         },
-        ref x if x ==  "\"AddTag\""=>{
+        "\"AddTag\""=>{
             println!("AddTag found");
             let ns = json_obj.replace("{\"runeType\":\"AddTag\",","{");
             let dc : AddTag = json::decode(ns.trim()).unwrap();
             return dc.into_box();
         },
-        ref x if x ==  "\"SummonMinion\""=>{
+        "\"SummonMinion\""=>{
             println!("SummonMinion found");
             let ns = json_obj.replace("{\"runeType\":\"SummonMinion\",","{");
             let dc : SummonMinion = json::decode(ns.trim()).unwrap();
             return dc.into_box();
         },
-        ref x if x ==  "\"PlayMinion\""=>{
+        "\"PlayMinion\""=>{
             println!("PlayMinion found");
             let ns = json_obj.replace("{\"runeType\":\"PlayMinion\",","{");
             let dc : PlayMinion = json::decode(ns.trim()).unwrap();
             return dc.into_box();
         },
-        ref x if x ==  "\"RemoveTag\""=>{
+        "\"RemoveTag\""=>{
             println!("RemoveTag found");
             let ns = json_obj.replace("{\"runeType\":\"RemoveTag\",","{");
             let dc : RemoveTag = json::decode(ns.trim()).unwrap();
             return dc.into_box();
         },
-        ref x if x ==  "\"SetHealth\""=>{
+        "\"SetHealth\""=>{
             println!("SetHealth found");
             let ns = json_obj.replace("{\"runeType\":\"SetHealth\",","{");
             let dc : SetHealth = json::decode(ns.trim()).unwrap();
             return dc.into_box();
         },
-        ref x if x ==  "\"ModifyHealth\""=>{
+        "\"ModifyHealth\""=>{
             println!("ModifyHealth found");
             let ns = json_obj.replace("{\"runeType\":\"ModifyHealth\",","{");
             let dc : ModifyHealth = json::decode(ns.trim()).unwrap();
             return dc.into_box();
         },
-        ref x if x ==  "\"SetAttack\""=>{
+        "\"SetAttack\""=>{
             println!("SetAttack found");
             let ns = json_obj.replace("{\"runeType\":\"SetAttack\",","{");
             let dc : SetAttack = json::decode(ns.trim()).unwrap();
             return dc.into_box();
         },
-        ref x if x ==  "\"ModifyAttack\""=>{
+        "\"ModifyAttack\""=>{
             println!("ModifyAttack found");
             let ns = json_obj.replace("{\"runeType\":\"ModifyAttack\",","{");
             let dc : ModifyAttack = json::decode(ns.trim()).unwrap();
             return dc.into_box();
         },
-        ref x if x ==  "\"DamageRune\""=>{
-            println!("ModifyAttack found");
+        "\"ModifyHeroHealth\""=>{
+            println!("ModifyHeroHealth found");
+            let ns = json_obj.replace("{\"runeType\":\"ModifyHeroHealth\",","{");
+            let dc : ModifyHeroHealth = json::decode(ns.trim()).unwrap();
+            return dc.into_box();
+        },
+        "\"DamageRune\""=>{
+            println!("ModifyHeroHealth found");
             let ns = json_obj.replace("{\"runeType\":\"DamageRune\",","{");
             let dc : DamageRune = json::decode(ns.trim()).unwrap();
             return dc.into_box();
         },
-        ref x if x ==  "\"KillMinion\""=>{
+        "\"KillMinion\""=>{
             println!("DamageRune found");
             let ns = json_obj.replace("{\"runeType\":\"KillMinion\",","{");
             let dc : KillMinion = json::decode(ns.trim()).unwrap();
